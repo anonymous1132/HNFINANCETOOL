3aa7b510e1efa69f8e4ccda1da2f351ed9b5e209
anonymous1132 <34846022+anonymous1132@users.noreply.github.com>
2018-08-10 20:34:49 +0800

    20180810

--- mui-master/1.0/FirstFloor.ModernUI/CaoJin.HNFinanceTool/Content/BudgetaryUpperLimitAppearance.xaml.cs
+++ mui-master/1.0/FirstFloor.ModernUI/CaoJin.HNFinanceTool/Content/BudgetaryUpperLimitAppearance.xaml.cs
@@ -55,5 +55,97 @@ namespace CaoJin.HNFinanceTool.Content
                 }
             }
         }
+
+        private void DG1_KeyDown(object sender, KeyEventArgs e)
+        {
+            System.Windows.Input.KeyboardDevice kd = e.KeyboardDevice;
+            if ((kd.GetKeyStates(Key.LeftCtrl) & kd.GetKeyStates(Key.V)) > 0 || (kd.GetKeyStates(Key.LeftCtrl) & kd.GetKeyStates(Key.V)) > 0)
+            {
+                DataGirdViewCellPaste();
+            }
+        }
+
+        private void DataGirdViewCellPaste()
+        {
+    
+                // 获取剪切板的内容，并按行分割  
+                string pasteText = Clipboard.GetText();
+                if (string.IsNullOrEmpty(pasteText))
+                    return;
+                int tnum = 0;
+                int nnum = 0;
+                //获得当前剪贴板内容的行、列数
+                for (int i = 0; i < pasteText.Length; i++)
+                {
+                    if (pasteText.Substring(i, 1) == "\t")
+                    {
+                        tnum++;
+                    }
+                    if (pasteText.Substring(i, 1) == "\n")
+                    {
+                        nnum++;
+                    }
+                }
+                Object[,] data;
+                //粘贴板上的数据来自于EXCEL时，每行末都有\n，在DATAGRIDVIEW内复制时，最后一行末没有\n
+                if (pasteText.Substring(pasteText.Length - 1, 1) == "\n")
+                {
+                    nnum = nnum - 1;
+
+                }
+
+                tnum = tnum / (nnum + 1);
+                data = new object[nnum + 1, tnum + 1];//定义一个二维数组
+
+                String rowstr;
+                rowstr = "";
+                //MessageBox.Show(pasteText.IndexOf("B").ToString());
+                //对数组赋值
+                for (int i = 0; i < (nnum + 1); i++)
+                {
+                    for (int colIndex = 0; colIndex < (tnum + 1); colIndex++)
+                    {
+                        //一行中的最后一列
+                        if (colIndex == tnum && pasteText.IndexOf("\r") != -1)
+                        {
+                            rowstr = pasteText.Substring(0, pasteText.IndexOf("\r"));
+                        }
+                        //最后一行的最后一列
+                        if (colIndex == tnum && pasteText.IndexOf("\r") == -1)
+                        {
+                            rowstr = pasteText.Substring(0);
+                        }
+                        //其他行列
+                        if (colIndex != tnum)
+                        {
+                            rowstr = pasteText.Substring(0, pasteText.IndexOf("\t"));
+                            pasteText = pasteText.Substring(pasteText.IndexOf("\t") + 1);
+                        }
+                        data[i, colIndex] = rowstr;
+                    }
+                    //截取下一行数据
+                    pasteText = pasteText.Substring(pasteText.IndexOf("\n") + 1);
+
+                }
+                //获取当前选中单元格所在的列序号
+                //int curntindex = DG1.CurrentRow.Cells.IndexOf(dataGridView1.CurrentCell);
+                int currentindex = DG1.CurrentCell.Column.DisplayIndex;
+                 string columnname = DG1.CurrentColumn.Header.ToString();
+                //获取获取当前选中单元格所在的行序号
+                //int rowindex = dataGridView1.CurrentRow.Index;
+                int rowindex = DG1.SelectedIndex;
+            if (columnname == "项目编号")
+            {
+                for(int i=0;i<data.GetLength(0);i++)
+                {
+                    obc_budgetary[rowindex+i].ProjectCode = data[i, 0].ToString();
+                    if (rowindex + i == obc_budgetary.Count-1) break;
+                }
+                
+            }
+
+        }
+
+
     }
 }
